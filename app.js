b59aaa79fb503290e8d28d38de8e58d76207521a
nozomi.hirayama <non0125.ccff@gmail.com>
2021-06-06 00:02:44 +0000

    base

---
+++ app.js
@@ -0,0 +1,39 @@
+const question = 'ゲーム史上、最も売れたゲーム機は次の内どれ？';
+const answers = [
+    'スーパーファミコン',
+    'プレイステーション2',
+    'ニンテンドースイッチ',
+    'ニンテンドーDS'
+    ];
+const correct = 'ニンテンドーDS';
+
+const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
+
+//クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent = question;
+    let buttonIndex = 0;
+    while(buttonIndex < buttonLength){
+        $button[buttonIndex].textContent = answers[buttonIndex];
+        buttonIndex++;
+    }
+}
+setupQuiz();
+
+const clickHandler = (e) => {
+    if(correct === e.target.textContent){
+        window.alert('正解！');
+    } else {
+        window.alert('不正解！');
+    }
+};    
+
+//ボタンをクリックしたら正誤判定
+let handlerIndex = 0;
+while (handlerIndex < buttonLength) {
+    $button[handlerIndex].addEventListener('click', (e)=> {
+        clickHandler(e);
+    });
+    handlerIndex++;
+}
