6ed7e14123570f73f1901c48079f501891e36af4
nozomi.hirayama <non0125.ccff@gmail.com>
2021-06-06 06:03:40 +0000

    基本設計の完了

--- app.js
+++ app.js
@@ -1,32 +1,69 @@
-const question = 'ゲーム史上、最も売れたゲーム機は次の内どれ？';
-const answers = [
-    'スーパーファミコン',
-    'プレイステーション2',
-    'ニンテンドースイッチ',
-    'ニンテンドーDS'
-    ];
-const correct = 'ニンテンドーDS';
+const quiz = [
+    {
+        question: '乃木坂46でドイツ出身なのは次のうち誰？',
+        answers: [
+    '白石麻衣',
+    '山下美月',
+    '秋元真夏',
+    '生田絵梨花'            
+     ],
+     correct: '生田絵梨花'
+    }, {
+        question: 'TWICEで韓国人じゃないのは次のうち誰？',
+        answers: [
+    'ジヒョ',
+    'ジョンヨン',
+    'ツウィ',
+    'ナヨン'            
+     ],
+     correct: 'ツウィ'
+    }, {
+        question: 'Niziuの中でアメリカ人とのハーフは次のうち誰？',
+        answers: [
+    'ニナ',
+    'リオ',
+    'リマ',
+    'リク'            
+     ],
+     correct: 'ニナ'
+    }
+];
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 
 //クイズの問題文、選択肢を定義
 const setupQuiz = () => {
-    document.getElementById('js-question').textContent = question;
+    document.getElementById('js-question').textContent = quiz[quizIndex].question
     let buttonIndex = 0;
     while(buttonIndex < buttonLength){
-        $button[buttonIndex].textContent = answers[buttonIndex];
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex++;
     }
 }
 setupQuiz();
 
 const clickHandler = (e) => {
-    if(correct === e.target.textContent){
+    if(quiz[quizIndex].correct === e.target.textContent){
         window.alert('正解！');
+        score++;
     } else {
         window.alert('不正解！');
     }
+    
+    quizIndex++;
+    if(quizIndex < quizLength){
+        //問題数がまだあればこちらを実行
+        setupQuiz();
+        } else {
+        //問題数がもうなければこちらを実行
+        window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
+        }
+    
 };    
 
 //ボタンをクリックしたら正誤判定
